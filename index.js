cc105d9efa958a3a3577d7c9a2606aabaaf6af6a
Jay Jajoo <84397446+JayJajoo@users.noreply.github.com>
2024-03-18 17:07:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server=app.listen(process.env.PORT,()=>{
 const io=socket(server,{
     cors:{
         origin:"https://viberz.netlify.app/",
-        credentials:true,
+        // credentials:true,
     }
 })
 
